128a3628c3a60c20369919300c88883642b36838
Hermela Bekele <hermelabekeleabebe@gmail.com>
2024-10-05 23:34:56 +0300

    update

---
+++ firebaseauth.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDOIjbyzVg4U_t5slL7sut16scOjvdFtLA",
+    authDomain: "login-47465.firebaseapp.com",
+    projectId: "login-47465",
+    storageBucket: "login-47465.appspot.com",
+    messagingSenderId: "380767607866",
+    appId: "1:380767607866:web:f4765f3213fb2b38e1a903"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
